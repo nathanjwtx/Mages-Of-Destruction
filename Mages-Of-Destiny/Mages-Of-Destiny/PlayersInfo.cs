9faeb9979fce58dabb4716fd87eac3b76659c090
Nathan Waterman <nathanjwuk@gmail.com>
2017-11-13 07:34:21 -0600

    Updated static class with jagged array plus retrieval method

--- Mages-Of-Destiny/Mages-Of-Destiny/PlayersInfo.cs
+++ Mages-Of-Destiny/Mages-Of-Destiny/PlayersInfo.cs
@@ -7,82 +7,94 @@ namespace Mages_Of_Destiny
     public static class PlayersInfo
     {
         static string playersInfoDirectory;
-        static int [][] fullInfo;
+        static string[][] fullInfo;
         static int [] scores;
         static int [] levels;
 
-//        EraseLevels()
-//        {}
-//        PrintLevels()
-//        {}
-//        RetrieveLevels()
-//        {}
-//        UpdateLevels()
-//        {}
-//        EraseScores()
-//        {}
-        //public static void PrintScores()
-        //{
-        //    foreach (var item in scores)
-        //    {
-        //        Console.WriteLine(item);
-        //    }
-        //}
+        //        EraseLevels()
+        //        {}
+        //        PrintLevels()
+        //        {}
+        //        RetrieveLevels()
+        //        {}
+        //        UpdateLevels()
+        //        {}
+        //        EraseScores()
+        //        {}
+        public static void PrintScores()
+        {
+            foreach (int x in scores)
+            {
+                Console.WriteLine(x);
+            }
+        }
 
-        //private static void RetrieveScores()
-        //{
-        //    for (int j = 0; j < 1; j++)
-        //    {
-        //        for (int i = 0; i < scores.Length; i++)
-        //        {
-        //            scores[i] = fullInfo[j, i];
-        //        }
-        //    }
-        //}
-        
+        private static void RetrieveScores()
+        {
+            scores = new int[6];
+            for (var i = 0; i < 6; i++)
+            {
+                scores[i] = Convert.ToInt32(fullInfo[0][i]);
+            }
+        }
+
         //public static void UpdateScores(int level)
         //{
         //    levels[0] = level;
         //}
-//        EraseFullInfo()
-//        {}
-//        PrintFullInfo()
-//        {}
-//        RetrieveFullInfo()
-//        {}
-//        UpdateFullInfo()
-//        {}
-//        Save()
-//        {}
+        //        EraseFullInfo()
+        //        {}
+        //        PrintFullInfo()
+        //        {}
+        //        RetrieveFullInfo()
+        //        {}
+        //        UpdateFullInfo()
+        //        {}
+        //        Save()
+        //        {}
 
         public static void Initialize()
         {
-            fullInfo = new int[][]
-            {
-                new int[] {1, 2, 3, 4, 5, 6 }, 
-                new int[] {7, 8, 9 , 10, 11, 12}
-            };
             //scores = new int[6];
             //RetrieveScores();
-            playersInfoDirectory = ($@"C:\Users\Nathan - User\OneDrive\Work - C Sharp\02 Udemy - TodV OOP with C#\Mages-Of-Destiny\Mages-Of-Destiny\");
-            if (File.Exists(playersInfoDirectory + "save.txt"))
+            //playersInfoDirectory = ($@"C:\Users\Nathan - User\OneDrive\Work - C Sharp\02 Udemy - TodV OOP with C#\Mages-Of-Destiny\Mages-Of-Destiny\");
+            playersInfoDirectory = ($@"C:\Users\watenatj\OneDrive\Work - C Sharp\02 Udemy - TodV OOP with C#\Mages-Of-Destiny\Mages-Of-Destiny\");
+            if (File.Exists(playersInfoDirectory + "save.txt") == false)
             {
+                fullInfo = new string[][]
+{
+                new string[] {"10", "20", "30", "40", "50", "60"},
+                new string[] {"70", "80", "90", "100", "110", "120"}
+};
                 Console.WriteLine("hello");
                 using (StreamWriter file = new StreamWriter(playersInfoDirectory + "save.txt"))
                 {
-                    foreach (int[] t in fullInfo)
+                    foreach (string[] t in fullInfo)
                     {
-                        foreach (int t1 in t)
+                        foreach (string t1 in t)
                         {
-                            Console.WriteLine(t1);
+                            file.WriteLine(t1);
                         }
                     }
+                    file.Close();
                 }
             }
             else
             {
-                File.Create(playersInfoDirectory + "save.txt");
+                fullInfo = new string[2][];
+                fullInfo[0] = new string[6];
+                fullInfo[1] = new string[6];
+                var x = 0;
+                for (var i = 0; i < 2; i++)
+                {
+                    for (var j = 0; j < 6; j++)
+                    {
+                        fullInfo[i][j] = File.ReadAllLines(playersInfoDirectory + "save.txt")[x];
+                        x += 1;
+                    }
+                }
             }
+            RetrieveScores();
         }
     }
 }
--- Mages-Of-Destiny/Mages-Of-Destiny/Program.cs
+++ Mages-Of-Destiny/Mages-Of-Destiny/Program.cs
@@ -13,7 +13,7 @@ namespace Mages_Of_Destiny
             var health = 40;
             var nathan = new Warrior("Nathan", 2);
             PlayersInfo.Initialize();
-            //PlayersInfo.PrintScores();
+            PlayersInfo.PrintScores();
 
         }
     }
